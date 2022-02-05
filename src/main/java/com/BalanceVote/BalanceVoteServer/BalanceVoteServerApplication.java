414eb8ad70cb6bcf2aee0aca3d61e09903b74c69
junman95 <junman95@naver.com>
2022-02-05 15:10:49 +0900

    Chore: Set new Java Spring server
    -Description-
    Balance vote platform for android
    -Version-
    Java Ver : 11
    Package manager : Gradle
    Spring Ver : 2.6.3
    Packaging : JAR

---
+++ src/main/java/com/BalanceVote/BalanceVoteServer/BalanceVoteServerApplication.java
@@ -0,0 +1,13 @@
+package com.BalanceVote.BalanceVoteServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceVoteServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalanceVoteServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BalanceVote/BalanceVoteServer/BalanceVoteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BalanceVote.BalanceVoteServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceVoteServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
