860a9e078453de9816fbe83d9d70d8f45022f956
Dong-Geon Lee <secmatth1996@gmail.com>
2022-03-05 00:03:24 +0900

    Add selecting most voted posts

--- src/main/java/com/BalanceVote/BalanceVoteServer/contorller/VoteController.java
+++ src/main/java/com/BalanceVote/BalanceVoteServer/contorller/VoteController.java
@@ -3,11 +3,11 @@ package com.BalanceVote.BalanceVoteServer.contorller;
 import java.util.List;
 
 import com.BalanceVote.BalanceVoteServer.entity.VotePost;
+import com.BalanceVote.BalanceVoteServer.repository.VotePostRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,15 +18,26 @@ import lombok.extern.slf4j.Slf4j;
 public class VoteController {
     
     @Autowired
-    private JpaRepository<VotePost, Long> votePostRepository;
+    private VotePostRepository votePostRepository;
 
     /**
      * Search all vote posts.
-     * @return Page<VotePost>
      * @author DongGeon Lee
      */
     @GetMapping("/post/vote-post")
     public Page<VotePost> getAllVotePost(Pageable pageable){
         return votePostRepository.findAll(pageable);
+    }
+
+    /**
+     * Get most voted posts in Desc order.
+     * @author DongGeon Lee
+     */
+    @GetMapping("/post/most-voted")
+    public List<VotePost> getMostVotedPostinPeriod(int count){
+        if (count < 1) {
+            count = 1;
+        }
+        return votePostRepository.findtwetgweg(count);
     }
 }
---
+++ src/main/java/com/BalanceVote/BalanceVoteServer/repository/VotePostRepository.java
@@ -0,0 +1,19 @@
+package com.BalanceVote.BalanceVoteServer.repository;
+
+import java.util.List;
+
+import com.BalanceVote.BalanceVoteServer.entity.VotePost;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+/**
+ * User Repositiory extending CrudRepository
+ * @author DongGeon Lee
+ */
+public interface VotePostRepository extends JpaRepository<VotePost, Long>{
+    
+    @Query(value="SELECT v.*, vote_cnt_1 + vote_cnt_2 AS tot_cnt FROM vote_post v ORDER BY tot_cnt DESC LIMIT :limit", nativeQuery=true)
+    List<VotePost> findtwetgweg(@Param("limit") int limit);
+}
