48bc11aacbe97a0997a41152a54a71acfc811b6c
junman95 <junman95@naver.com>
2022-03-06 16:55:47 +0900

    feature : Add api that gets specific post's parent comment list

--- src/main/java/com/BalanceVote/BalanceVoteServer/contorller/CommentController.java
+++ src/main/java/com/BalanceVote/BalanceVoteServer/contorller/CommentController.java
@@ -6,15 +6,20 @@ import com.BalanceVote.BalanceVoteServer.entity.ParentComment;
 import com.BalanceVote.BalanceVoteServer.repository.ParentCommentRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController // Rest api annotation
 @Slf4j // logging anotation
 public class CommentController {
     @Autowired
     private ParentCommentRepository parentCommentRepository;
+
+    @GetMapping("/comment/get-post-parent-comment/{postId}")
+    public List<ParentComment> getPostParentComment(@PathVariable String postId){
+        return parentCommentRepository.findAllByPostId(postId);
+    }
 
     @PostMapping("/comment/create-parent-comment")
     public ParentComment addParentComment(@RequestBody ParentCommentForm dto){
--- src/main/java/com/BalanceVote/BalanceVoteServer/repository/ParentCommentRepository.java
+++ src/main/java/com/BalanceVote/BalanceVoteServer/repository/ParentCommentRepository.java
@@ -5,7 +5,9 @@ import com.BalanceVote.BalanceVoteServer.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public interface ParentCommentRepository extends JpaRepository<ParentComment,Long> {
+
+    List<ParentComment> findAllByPostId(String postId);
 }
