507084a0a3b6eea99a6a2d669a6c3745affa2ac9
junman95 <junman95@naver.com>
2022-03-09 19:53:59 +0900

    feature : fix findUserComments to get uuid parameter

--- src/main/java/com/BalanceVote/BalanceVoteServer/repository/ParentCommentRepository.java
+++ src/main/java/com/BalanceVote/BalanceVoteServer/repository/ParentCommentRepository.java
@@ -6,14 +6,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
-
+/**
+ * ParentComment Repositiory extending CrudRepository
+ * @author JunmanChoi
+ */
 public interface ParentCommentRepository extends JpaRepository<ParentComment,Long> {
 
     List<ParentComment> findAllByPostId(String postId);
 
-    @Query(value = "SELECT parent.* FROM balance_vote_database.parent_comment parent LEFT OUTER JOIN balance_vote_database.child_comment child ON child.parent_id = parent.id ORDER BY time_stamp DESC", nativeQuery = true)
-    List<ParentComment> findUserComments(String uuid);
+    @Query(value = "SELECT parent.* FROM balance_vote_database.parent_comment parent LEFT OUTER JOIN balance_vote_database.child_comment child ON child.parent_id = parent.id WHERE parent.uuid = :uuid ORDER BY time_stamp DESC", nativeQuery = true)
+    List<ParentComment> findUserComments(@Param(value = "uuid") String uuid);
 
     List<ParentComment> findAllByUuid(String uuid);
 }
