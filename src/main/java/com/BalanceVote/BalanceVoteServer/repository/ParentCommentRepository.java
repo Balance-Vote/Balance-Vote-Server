eb8594fb23a2c3f9d8f457b44cddd1dc136983ff
junman95 <junman95@naver.com>
2022-03-09 19:50:30 +0900

    feature : update findUserComments Query to order records by recent time stamp

--- src/main/java/com/BalanceVote/BalanceVoteServer/repository/ParentCommentRepository.java
+++ src/main/java/com/BalanceVote/BalanceVoteServer/repository/ParentCommentRepository.java
@@ -12,7 +12,7 @@ public interface ParentCommentRepository extends JpaRepository<ParentComment,Lon
 
     List<ParentComment> findAllByPostId(String postId);
 
-    @Query(value = "SELECT parent.* FROM balance_vote_database.parent_comment parent LEFT OUTER JOIN balance_vote_database.child_comment child ON child.parent_id = parent.id", nativeQuery = true)
+    @Query(value = "SELECT parent.* FROM balance_vote_database.parent_comment parent LEFT OUTER JOIN balance_vote_database.child_comment child ON child.parent_id = parent.id ORDER BY time_stamp DESC", nativeQuery = true)
     List<ParentComment> findUserComments(String uuid);
 
     List<ParentComment> findAllByUuid(String uuid);
