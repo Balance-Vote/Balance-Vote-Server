b1ef23c0c13a8084d0eb9c776e49fe933947301e
junman95 <junman95@naver.com>
2022-03-15 23:33:41 +0900

    chore: fix spelling issue

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,4 +11,4 @@ JAR_NAME=$(ls $REPOSITORY/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/$JAR_NAME
 
 echo "> $JAR_PATH deploy"
-nohup java -jar $JAR_PATH > /deploy/nohup.log 2>&1 &
+nohup java -jar $JAR_PATH 2>&1 &
