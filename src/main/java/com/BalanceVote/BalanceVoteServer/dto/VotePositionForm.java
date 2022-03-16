33d59e129dd3a900a005a62c56b183252cf7b773
Dong-Geon Lee <secmatth1996@gmail.com>
2022-03-17 01:38:04 +0900

    feature: Add VotePositionForm

---
+++ src/main/java/com/BalanceVote/BalanceVoteServer/dto/VotePositionForm.java
@@ -0,0 +1,24 @@
+package com.BalanceVote.BalanceVoteServer.dto;
+
+import com.BalanceVote.BalanceVoteServer.entity.VotePosition;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * VotePositionCreation DTO
+ * @author DongGeon Lee
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VotePositionForm {
+    private String selectedPos;
+    private String postId;
+    private String uuid;
+
+    public VotePosition toEntity(){
+        return new VotePosition(null, selectedPos, postId, uuid);
+    }
+}
