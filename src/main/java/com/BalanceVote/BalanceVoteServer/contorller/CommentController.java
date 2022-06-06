6290b4305e1a3d5c24d3e03917abd7aa42a9e5a9
junman95 <junman95@naver.com>
2022-06-06 18:16:27 +0900

    fix: change type of post-id from Long to String create query for getAllByPostId at ChildCommentRepository

--- src/main/java/com/BalanceVote/BalanceVoteServer/contorller/CommentController.java
+++ src/main/java/com/BalanceVote/BalanceVoteServer/contorller/CommentController.java
@@ -60,7 +60,7 @@ public class CommentController {
     @GetMapping("/comment/get-comment/child/parent-id/{parentCmtId}")
     @ApiOperation(value = "대댓글 조회", notes = "특정 부모 댓글(1차 댓글)의 자식 댓글(대댓글) 조회")
     @ApiImplicitParam(name="parentCmtId", required = true,value = "부모 댓글 ID",dataType = "int")
-    public List<ChildComment> getChildComment(@PathVariable String parentCmtId){
+    public List<ChildComment> getChildComment(@PathVariable Long parentCmtId){
         List<ChildComment> childComments = childCommentRepository.findAllByParentCmtId(parentCmtId);
         return childComments;
     }
